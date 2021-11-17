fc617a6ea90fc0b48f44d0f123e9c3a84627e295
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-17 17:27:02 +0100

    MILESTONE 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container py-4">
+      <div class="input-group w-50">
+        <input
+          type="search"
+          id="searchForm"
+          class="form-control"
+          v-model="searchQuery"
+          @keyup.enter="apiCall('movie', searchQuery)"
+        /><button
+          type="button"
+          class="btn btn-dark"
+          @click="apiCall('movie', searchQuery)"
+        >
+          Cerca
+        </button>
+      </div>
+    </div>
+    <div class="container">
+      <ul>
+        <li v-for="(item, i) in apiData.movie" :key="i">
+          {{
+          `titolo: ${item.title}, 
+          titolo originale: ${item.original_title},
+          lingua: ${item.original_language},
+          voto: ${item.vote_average}.`,
+          }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      // codice personale
+      apiKey: "4e887be83ce5c8ffc309c0a492e50a7f",
+      apiUrl: "https://api.themoviedb.org/3",
+      apiUrlEnd: {
+        movie: "/search/movie",
+        tv: "/search/tv",
+      },
+      apiData: {
+        movie: [],
+        tv: [],
+      },
+      searchQuery: "",
+    };
   },
+  methods: {
+    apiCall(urlEnd, query) {
+      axios
+        .get(this.apiUrl + this.apiUrlEnd[urlEnd], {
+          params: {
+            api_key: this.apiKey,
+            query,
+          },
+        })
+        .then((resp) => {
+          this.apiData[urlEnd] = resp.data.results;
+        });
+    },
+  },
+  mounted() {},
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/scss/bootstrap";
 </style>
